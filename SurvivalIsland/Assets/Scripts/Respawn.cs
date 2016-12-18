254523abe10bf98074fb49a87b6df215cdec74bd
Iván Manso Arenal <ivan.manso@babel-si.local>
2016-12-18 18:40:45 +0100

    v1.0.0

---
+++ SurvivalIsland/Assets/Scripts/Respawn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class  Respawn : MonoBehaviour
+{
+
+    public List<GameObject> animals;
+    public float spawnTime = 5f;
+    public Transform[] spawnPoints;
+
+    // Use this for initialization
+    void Start()
+    {
+        animals = new List<GameObject>();
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+/*        if (playerHealth.currentHealth <= 0)
+        {
+            return;
+        }*/
+        int index = Random.Range (0, animals.Count);
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(animals[index], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+
+}
---
+++ SurvivalIsland/Assets/Scripts/RespawnWater.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class  RespawnWater : MonoBehaviour
+{
+
+    public List<GameObject> fishes;
+    public float spawnTime = 5f;
+    public Transform[] spawnPoints;
+
+    // Use this for initialization
+    void Start()
+    {
+        fishes = new List<GameObject>();
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+/*        if (playerHealth.currentHealth <= 0)
+        {
+            return;
+        }*/
+        int index = Random.Range (0, fishes.Count);
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(fishes[index], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+
+}
